100b617e92c2e2d586739b5e25c7588bb37fa48e
Anton Malyshev <antouske@gmail.com>
2016-01-10 18:47:49 +0200

    Added publish and delete post buttons

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -36,3 +36,17 @@ def post_edit(request, pk):
         else:
             form = PostForm(instance=post)
         return render(request, 'blog/post_edit.html', {'form': form})
+
+def unpublished(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/unpublished.html', {'posts': posts})
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('blog.views.post_detail', pk=pk)
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('blog.views.post_list')
